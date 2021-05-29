15d06ea3f54cf9376b4e445256749ede7d423cb3
jorgevillay <jorge_iv94@hotmail.com>
2021-05-28 22:56:35 -0500

    Removed original files and added global styles

---
+++ src/utils/globalStyles.js
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+
+const globalStyles = css`
+  body {
+    font-family: 'Questrial', sans-serif;
+    font-size: 17px;
+  }
+`;
+
+export default globalStyles;
